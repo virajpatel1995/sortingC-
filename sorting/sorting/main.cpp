fa4275f9a116b75ef2ce0ac0421c881fbdf8c301
viraj patel <patelviraj2@yahoo.com>
2016-11-14 20:01:24 -0500

    linked to the xcode

---
+++ sorting/sorting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sorting
+//
+//  Created by Viraj patel on 11/14/16.
+//  Copyright © 2016 Viraj Patel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
